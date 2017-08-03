73292e98f27cb758b919102f6955f454c4698a17
cricket-programmer <cricket.programmer@gmail.com>
2017-08-03 11:49:15 -0700

    Trust v2.0 Update

---
+++ footer.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The template for displaying the footer.
+ */
+
+				dentario_close_wrapper();	// <!-- </.content> -->
+
+				dentario_profiler_add_point(esc_html__('After Page content', 'dentario'));
+	
+				// Show main sidebar
+				get_sidebar();
+
+				if (dentario_get_custom_option('body_style')!='fullscreen') dentario_close_wrapper();	// <!-- </.content_wrap> -->
+				?>
+			
+			</div>		<!-- </.page_content_wrap> -->
+			
+			<?php
+			// Footer Testimonials stream
+			require_once dentario_get_file_dir('templates/_parts/footer-testimonials.php');
+
+
+			// Footer sidebar
+			require_once dentario_get_file_dir('templates/_parts/footer-sidebar.php');
+
+
+			// Footer Twitter stream
+			require_once dentario_get_file_dir('templates/_parts/twitter-stream.php');
+
+
+			// Google map
+			require_once dentario_get_file_dir('templates/_parts/google-map.php');
+			
+
+			// Copyright area
+			require_once dentario_get_file_dir('templates/_parts/copyright-area.php');
+			
+
+			dentario_profiler_add_point(esc_html__('After Footer', 'dentario'));
+			?>
+			
+		</div>	<!-- /.page_wrap -->
+
+	</div>		<!-- /.body_wrap -->
+	
+	<?php if ( !dentario_param_is_off(dentario_get_custom_option('show_sidebar_outer')) ) { ?>
+	</div>	<!-- /.outer_wrap -->
+	<?php } ?>
+
+<?php
+// Post/Page views counter
+get_template_part(dentario_get_file_slug('templates/_parts/views-counter.php'));
+
+// Login/Register
+if (dentario_get_theme_option('show_login')=='yes') {
+	dentario_enqueue_popup();
+	// Anyone can register ?
+	if ( (int) get_option('users_can_register') > 0) {
+		get_template_part(dentario_get_file_slug('templates/_parts/popup-register.php'));
+	}
+	get_template_part(dentario_get_file_slug('templates/_parts/popup-login.php'));
+}
+
+// Front customizer
+if (dentario_get_custom_option('show_theme_customizer')=='yes') {
+	require_once trailingslashit( get_template_directory() ) .'fw/core/core.customizer/front.customizer.php';
+}
+?>
+
+<a href="#" class="scroll_to_top icon-up" title="<?php esc_attr_e('Scroll to top', 'dentario'); ?>"></a>
+
+<div class="custom_html_section">
+<?php echo force_balance_tags(dentario_get_custom_option('custom_code')); ?>
+</div>
+
+<?php
+echo force_balance_tags(dentario_get_custom_option('gtm_code2'));
+
+dentario_profiler_add_point(esc_html__('After Theme HTML output', 'dentario'));
+
+wp_footer(); 
+?>
+
+
+</body>
+</html>
--- themes/dentario/header.php
+++ themes/dentario/header.php
@@ -20,6 +20,14 @@ $video_bg_show  = dentario_get_custom_option('show_video_bg')=='yes' && (dentari
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="<?php echo 'scheme_' . esc_attr($body_scheme); ?>">
 <head>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5J7ZNVP');</script>
+		<!-- End Google Tag Manager -->
+
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1<?php if (dentario_get_theme_option('responsive_layouts')=='yes') echo ', maximum-scale=1'; ?>">
@@ -44,65 +52,6 @@ $video_bg_show  = dentario_get_custom_option('show_video_bg')=='yes' && (dentari
 
 ?>
 
-<?php
-
-	if(is_home) {
-		echo '
-	<script type="application/ld+json">
-{
-  "@context": "http://schema.org",
-  "@type": "LocalBusiness",
-  "image": "https://trustdentalcare.com/wp-content/uploads/2016/12/trust-dental-care-logo.png",
-  "@id": "https://trustdentalcare.com",
-  "name": "Trust Dental Care",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "Blvd. General Rodolfo Sánchez Taboada 929",
-    "addressLocality": "Tijuana",
-    "addressRegion": "TJ",
-    "postalCode": "22000",
-    "addressCountry": "MX"
-  },
-  "geo": {
-    "@type": "GeoCoordinates",
-    "latitude": 32.533909,
-    "longitude": -117.0332227
-  },
-  "url": "https://trustdentalcare.com",
-  "telephone": "8448487878",
-  "openingHoursSpecification": [
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday"
-      ],
-      "opens": "08:30",
-      "closes": "18:00"
-    },
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": "Saturday",
-      "opens": "08:30",
-      "closes": "14:00"
-    },
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": "Sunday",
-      "opens": "closed",
-      "closes": "closed"
-    }
-  ]
-}
-</script>
-';
-	}
-
- ?>
-
  <script type="application/ld+json">
 {
   "@context": "http://schema.org",
@@ -115,11 +64,6 @@ $video_bg_show  = dentario_get_custom_option('show_video_bg')=='yes' && (dentari
     "streetAddress": "Boulevard General Rodolfo Sánchez Taboada 929,"
   },
   "image": "https://trustdentalcare.com/wp-content/uploads/2016/12/trust-dental-care-logo.png",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "5",
-    "reviewCount": "1742"
-  },
   "name": "Trust Dental Care",
   "openingHours": [
     "Mo-Fr 09:00-18:00",
@@ -152,6 +96,11 @@ wp_head();
 ?>
 </head>
 <body <?php body_class();?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5J7ZNVP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!-- facebook plugin -->
 <div id="fb-root"></div>
@@ -199,27 +148,7 @@ wp_head();
 			}
 	?>
 
-	<script type="text/javascript">
-		function messengerHandle() {
-		var messengerBtn = document.getElementById('messenger-btn');
-		var messengerTab = document.getElementById('messenger-tab');
-
-
-		if (messengerTab.classList.contains('fb-message-hide')) {
-			messengerTab.classList.remove('fb-message-hide');
-			messengerTab.classList.add('fb-message-show');
-		} else {
-			messengerTab.classList.remove('fb-message-show');
-			messengerTab.classList.add('fb-message-hide');
-		}
-
-
-		console.log('hola');
-		console.log(messengerTab);
-		}
-
-	</script>
-
+	
 
 
 
